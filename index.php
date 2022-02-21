a72a4f26b6af73da46fd85e27dc28efae0fb3c30
austin316dsouza <89637681+austin316dsouza@users.noreply.github.com>
2022-02-21 23:55:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,9 +17,12 @@
                 margin:auto 45%;
                 padding:100px auto;
                 /* background-color: red; */
-                border:5px solid red;
+                /* border:5px solid red; */
                 
             }
+         #result{
+             color:Yellow;
+         }   
 
 
     </style>
@@ -27,9 +30,12 @@
 <body>
     <div class="container">
     <form action="" method="post">
-        <input type="text" name="email" id="email">
+        <input type="text" name="email" id="email" placeholder="Email to add/search">
         <button type="submit" name="submit">Add</button>
         <button type="submit" name="search">Search</button>
+        <input type="number" name="emailUpdateId" placeholder="ID to update">
+        <input type="text" name="emailUpdate" placeholder="updated email">
+        <button type="submit" name="update">update</button>
     </form>
     </div>
    
@@ -42,8 +48,10 @@
             $query_to_add = "insert into email (email) values ('". $_POST['email']."');";
             if(mysqli_query($conn,$query_to_add))
             {
-                echo $_POST['email'] ."  ( Added recently)";
-                ?> <br> <?php
+                
+                ?> 
+                <h1 id="result"><?php echo $_POST['email'] ."  ( Added recently)"; ?></h1>
+                <br> <?php
             }
             else
             {
@@ -64,9 +72,11 @@
             {   
                 while($row = mysqli_fetch_assoc($result))
                 {   
-                    echo $row['email'];
+                    
     
-                    ?> <br> <?php
+                    ?> 
+                    <h1 id="result"><?php echo $row['email']." FOUND! ID = ".$row['id']; ?></h1>
+                    <br> <?php
                 }
             }
             else{
@@ -74,17 +84,51 @@
             }
         }
 
+        // if(isset($_POST['search']))
+        // {
+        //     // echo $_POST['email'] ."  (Added recently)";
+        //     $query_to_search = "select * from email where email like '". $_POST['email']."';";
+        //     //$result=mysqli_query($conn,$query_to_search);
+        //     if(mysqli_num_rows(mysqli_query($conn,$query_to_search))>0)
+        //     {   
+        //         while($row = mysqli_fetch_assoc(mysqli_query($conn,$query_to_search)))
+        //         {   
+        //             echo $row['email'];
+    
+        //             ?> <br> <?php
+        //         }
+        //     }
+        //     else{
+        //         echo "Not Found";
+        //     }
+        // }
+
+
+        if(isset($_POST['update']))
+        {   
+            //echo $_POST['email-update'];
+            $query_to_update = "update email set email='".$_POST['emailUpdate']."' where id='".$_POST['emailUpdateId']."'; ";
+            if(mysqli_query($conn,$query_to_update))
+            {
+                ?> 
+                    <h1 id="result"><?php echo "UPDATED ID ".$_POST['emailUpdateId']; ?></h1>
+                    <br> <?php
+            }
+            else{
+                echo $query_to_update." error: ".mysqli_error($conn);
+            }
+        }
 
 
 
 
-        $query_to_fetch="select email from email;";
+        $query_to_fetch="select * from email;";
         $result=mysqli_query($conn,$query_to_fetch);
         if(mysqli_num_rows($result)>0)
-        {   
+        {   ?> <h3>ID : EMAIL</h3> <?php
             while($row = mysqli_fetch_assoc($result))
             {   
-                echo $row['email'];
+                echo $row['id']." : ".$row['email'];
 
                 ?> <br> <?php
             }
